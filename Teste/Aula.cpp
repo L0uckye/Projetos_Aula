11053432ae79a241808a837a78404a824dbed654
L0uckye <130511810+L0uckye@users.noreply.github.com>
2024-05-27 23:18:35 -0300

    Adição de coração
    Adicionei meu código para printar um coração

---
+++ Teste/Aula.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "***************" << endl << "***  *****  ***" << endl << "**    ***    **\n";
+    cout << "**     *     **" << endl << "*      *      *" << endl << "*             *\n";
+    cout << "*             *" << endl << "*             *" << endl << "**           **\n";
+    cout << "**           **" << endl << "***         ***" << endl << "****       ****\n";
+    cout << "*****     *****" << endl << "******   ******" << endl << "******* *******\n";
+    cout << "***************";
+
+    return 0;
+}
